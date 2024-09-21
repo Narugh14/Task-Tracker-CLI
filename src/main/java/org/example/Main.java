44a9406ab5ff9f31738f7581292f4736db71ee16
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2024-09-21 16:59:39 -0600

    Lectura de un JSON
    Se desarrollo metodo JsonExist y JsonAllList, mostrando toda el JSON

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,37 @@
 package org.example;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 
 public class Main {
+
+
+
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        String filepath = "task.json";
+
+
+            JsonAllList(JsonExist(filepath));
+
+    }
+    private static StringBuilder JsonExist(String filepath){
+        StringBuilder jsonTask = new StringBuilder();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
+            String line;
+            while( (line = br.readLine()) != null){
+                jsonTask.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return jsonTask.append("");
+        }
+        return jsonTask;
+    }
+
+   private static void JsonAllList(StringBuilder AllTask){
+        System.out.println("El contenido del archivo JSON es: ");
+        System.out.println(AllTask);
     }
 }
