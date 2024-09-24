b4b7f8245a82736057e7d3c0d3696cc0bdbb22dc
Narugh14 <39018795+Narugh14@users.noreply.github.com>
2024-09-24 10:18:46 -0600

    Modificaciones para separar logicas
    se modificaron logicas y se agregaron commandos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,40 +1,46 @@
 package org.example;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.Scanner;
 
 
 public class Main {
 
-
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-       // String filepath = "task.json";
+        String commando;
 
-        String accion = scanner.nextLine();
-        TaskManager.ManagerTask(accion);
+        System.out.println("This is your Task tracker \n W. 'help' for see commands \n W. 'exit' for go out \n W. 'task' for see task tracker ");
+        while(true){
+            System.out.println(">");
+            commando = scanner.nextLine().trim();
 
-         //   JsonAllList(JsonExist(filepath));
-
-    }
-    private static StringBuilder JsonExist(String filepath){
-        StringBuilder jsonTask = new StringBuilder();
-
-        try(BufferedReader br = new BufferedReader(new FileReader(filepath))){
-            String line;
-            while( (line = br.readLine()) != null){
-                jsonTask.append(line);
+            if(commando.equalsIgnoreCase("exit")){
+                System.out.println("Exit...");
+                break;
+            } else if (commando.equalsIgnoreCase("help")) {
+                ListCommands(false);
+            }else if (commando.startsWith("task")) {
+                ListCommands(true);
+            }else if (commando.startsWith("add") || commando.startsWith("delete")||commando.startsWith("update")||commando.startsWith("list")){
+                ManagerTask.optionTask(commando);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return jsonTask.append("");
         }
-        return jsonTask;
+
     }
 
-   private static void JsonAllList(StringBuilder AllTask){
-        System.out.println("El contenido del archivo JSON es: ");
-        System.out.println(AllTask);
+    private static void ListCommands(boolean num) {
+            System.out.println("Commands for Task ");
+            System.out.println("---------------------");
+            System.out.println("add 'Task f/add' ");
+            System.out.println("delete 'Task f/add' ");
+            System.out.println("update 'Task f/add' ");
+            System.out.println("list 'Task f/add' ");
+        if (num == false) {
+            System.out.println("---------------------");
+            System.out.println("Commands for list");
+            System.out.println("---------------------");
+            System.out.println("list done");
+            System.out.println("list all");
+            System.out.println("list in-progress");
+        }
     }
 }
---
+++ src/main/java/org/example/ManagerTask.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class ManagerTask {
+
+    static void optionTask(String commands){
+        if(commands.startsWith("add")){
+            optionAdd(commands);
+        }
+    }
+
+    private static void optionAdd(String commands) {
+        
+    }
+}
